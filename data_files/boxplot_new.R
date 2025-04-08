6408fdf3d254794affa33abff538aa607b45f8bf
Vera <stepankverat@natur.cuni.cz>
2025-04-08 15:55:52 +0200

    boxplot_new funguje pro ploting TPM podle cross_spe

--- data_files/boxplot_new.R
+++ data_files/boxplot_new.R
@@ -4,23 +4,22 @@ knitr::opts_chunk$set(
   warning = FALSE,
   error = TRUE
 )
-# Load libraries
+
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-# Load and reshape expression data
-expression_matrix <- read.csv("your_data.csv", row.names = 1, check.names = FALSE)
-metadata <- read.csv("your_metadata.csv")  # must contain 'sample_id' and 'grouping'
-
+setwd("/home/verozvest/Documents/genomics_practical01/data_files")
+expression_matrix <- read.csv("imprinted_tpm.csv", row.names = 1, check.names = FALSE)
+metadata <- read.csv("metadata.csv") 
 
 expression_matrix$gene_id <- rownames(expression_matrix)
-expression_long <- pivot_longer(expression_matrix, -gene_id, names_to = "sample_id", values_to = "TPM")
-expression_annotated <- expression_long %>% left_join(metadata, by = "sample_id")
+expression_long <- pivot_longer(expression_matrix, -gene_id, names_to = "sample", values_to = "TPM")
+expression_annotated <- expression_long %>% left_join(metadata, by = "sample")
 expression_annotated <- expression_annotated %>% mutate(log_expression = log2(TPM + 1))
 
-# Boxplot to visualize gene expression per sample group
-ggplot(expression_annotated, aes(x = grouping, y = log_expression, fill = grouping)) +
+
+ggplot(expression_annotated, aes(x = cross_spe, y = log_expression, fill = cross_spe)) +
   geom_boxplot(outlier.size = 0.5, alpha = 0.7) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
